65f5f358cc1c116c604634017113c1e98e576a31
dai <dai12.gmail.com>
2022-09-02 11:33:11 +0800

    flex: rn offical demo 2

--- App.js
+++ App.js
@@ -1,137 +1,137 @@
 //---------RN offical flex demo 1 --------
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+// import React from "react";
+// import { StyleSheet, Text, View } from "react-native";
 
-const Flex = () => {
-  return (
-    <View style={[styles.container, {
-      // Try setting `flexDirection` to `"row"`.
-      flexDirection: "column"
-    }]}>
-      <View style={{ flex: 1, backgroundColor: "red" }} />
-      <View style={{ flex: 2, backgroundColor: "darkorange" }} />
-      <View style={{ flex: 3, backgroundColor: "green" }} />
-    </View>
-  );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 20,
-  },
-});
-
-export default Flex;
-
-
-// -------------*****RN offical flex demo2------------
-// import { useState } from 'react';
-// import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
-
-// const FlexDirectionBasics = () => {
-//   const [flexDirection, setflexDirection] = useState("column");
-
+// const Flex = () => {
 //   return (
-//     <PreviewLayout
-//       label="flexDirection"
-//       values={["column", "row", "row-reverse", "column-reverse"]}
-//       selectedValue={flexDirection}
-//       setSelectedValue={setflexDirection}
-//     >
-//       <View
-//         style={[styles.box, { backgroundColor: "powderblue" }]}
-//       />
-//       <View
-//         style={[styles.box, { backgroundColor: "skyblue" }]}
-//       />
-//       <View
-//         style={[styles.box, { backgroundColor: "steelblue" }]}
-//       />
-//     </PreviewLayout>
+//     <View style={[styles.container, {
+//       // Try setting `flexDirection` to `"row"`.
+//       flexDirection: "column"
+//     }]}>
+//       <View style={{ flex: 1, backgroundColor: "red" }} />
+//       <View style={{ flex: 2, backgroundColor: "darkorange" }} />
+//       <View style={{ flex: 3, backgroundColor: "green" }} />
+//     </View>
 //   );
 // };
-
-// const PreviewLayout = ({
-//   label,
-//   children,
-//   values,
-//   selectedValue,
-//   setSelectedValue,
-// }) => (
-//   <View style={{ padding: 10, flex: 1 }}>
-//     <Text style={styles.label}>{label}</Text>
-//     <View style={styles.row}>
-//       {values.map((value) => (
-//         <TouchableOpacity
-//           key={value}
-//           onPress={() => setSelectedValue(value)}
-//           style={[
-//             styles.button,
-//             selectedValue === value && styles.selected,
-//           ]}
-//         >
-//           <Text
-//             style={[
-//               styles.buttonLabel,
-//               selectedValue === value && styles.selectedLabel,
-//             ]}
-//           >
-//             {value}
-//           </Text>
-//         </TouchableOpacity>
-//       ))}
-//     </View>
-//     <View style={[styles.container, { [label]: selectedValue }]}>
-//       {children}
-//     </View>
-//   </View>
-// );
 
 // const styles = StyleSheet.create({
 //   container: {
 //     flex: 1,
-//     marginTop: 8,
-//     backgroundColor: "aliceblue",
-//   },
-//   box: {
-//     width: 50,
-//     height: 50,
-//   },
-//   row: {
-//     flexDirection: "row",
-//     flexWrap: "wrap",
-//   },
-//   button: {
-//     paddingHorizontal: 8,
-//     paddingVertical: 6,
-//     borderRadius: 4,
-//     backgroundColor: "oldlace",
-//     alignSelf: "flex-start",
-//     marginHorizontal: "1%",
-//     marginBottom: 6,
-//     minWidth: "48%",
-//     textAlign: "center",
-//   },
-//   selected: {
-//     backgroundColor: "coral",
-//     borderWidth: 0,
-//   },
-//   buttonLabel: {
-//     fontSize: 12,
-//     fontWeight: "500",
-//     color: "coral",
-//   },
-//   selectedLabel: {
-//     color: "white",
-//   },
-//   label: {
-//     textAlign: "center",
-//     marginBottom: 10,
-//     fontSize: 24,
+//     padding: 20,
 //   },
 // });
 
-// export default FlexDirectionBasics;
+// export default Flex;
+
+
+// -------------*****RN offical flex demo2------------
+import { useState } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+const FlexDirectionBasics = () => {
+  const [flexDirection, setflexDirection] = useState("column");
+
+  return (
+    <PreviewLayout
+      label="flexDirection"
+      values={["column", "row", "row-reverse", "column-reverse"]}
+      selectedValue={flexDirection}
+      setSelectedValue={setflexDirection}
+    >
+      <View
+        style={[styles.box, { backgroundColor: "powderblue" }]}
+      />
+      <View
+        style={[styles.box, { backgroundColor: "skyblue" }]}
+      />
+      <View
+        style={[styles.box, { backgroundColor: "steelblue" }]}
+      />
+    </PreviewLayout>
+  );
+};
+
+const PreviewLayout = ({
+  label,
+  children,
+  values,
+  selectedValue,
+  setSelectedValue,
+}) => (
+  <View style={{ padding: 10, flex: 1 }}>
+    <Text style={styles.label}>{label}</Text>
+    <View style={styles.row}>
+      {values.map((value) => (
+        <TouchableOpacity
+          key={value}
+          onPress={() => setSelectedValue(value)}
+          style={[
+            styles.button,
+            selectedValue === value && styles.selected,
+          ]}
+        >
+          <Text
+            style={[
+              styles.buttonLabel,
+              selectedValue === value && styles.selectedLabel,
+            ]}
+          >
+            {value}
+          </Text>
+        </TouchableOpacity>
+      ))}
+    </View>
+    <View style={[styles.container, { [label]: selectedValue }]}>
+      {children}
+    </View>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 8,
+    backgroundColor: "aliceblue",
+  },
+  box: {
+    width: 50,
+    height: 50,
+  },
+  row: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+  },
+  button: {
+    paddingHorizontal: 8,
+    paddingVertical: 6,
+    borderRadius: 4,
+    backgroundColor: "oldlace",
+    alignSelf: "flex-start",
+    marginHorizontal: "1%",
+    marginBottom: 6,
+    minWidth: "48%",
+    textAlign: "center",
+  },
+  selected: {
+    backgroundColor: "coral",
+    borderWidth: 0,
+  },
+  buttonLabel: {
+    fontSize: 12,
+    fontWeight: "500",
+    color: "coral",
+  },
+  selectedLabel: {
+    color: "white",
+  },
+  label: {
+    textAlign: "center",
+    marginBottom: 10,
+    fontSize: 24,
+  },
+});
+
+export default FlexDirectionBasics;
 
 //--------------demo3----------
