98769901f81a5e49860c0b15865a5e3d7175a4fc
bagguo <bagguo23@gmail.com>
2022-11-02 14:52:24 +0800

    add web test net

--- src/webview/MyWebComponent.js
+++ src/webview/MyWebComponent.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StatusBar, StyleSheet, Text, View } from 'react-native';
 import { WebView } from 'react-native-webview';
 
 class MyWebComponent extends Component {
   render() {
     return (
       <SafeAreaView style={{ flex: 1 }}>
-        <WebView style={{ flex: 1 }} source={{ uri: 'http://121.5.173.139:9800/' }} />
+        {/* <WebView style={{ flex: 1 }} source={{ uri: 'http://121.5.173.139:9800/' }} /> */}
+        <WebView style={{ flex: 1 }} source={{ uri: 'http://121.5.173.139:8080/' }} />
+        <StatusBar/>
       </SafeAreaView>
     );
   }
